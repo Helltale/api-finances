22726c6e7dc958080084d3d9b3baeedc10296611
Dmitry Andreev <andreev.dp@bk.ru>
2024-10-17 09:58:55 +0400

    fix: change struct IncomeExpected

--- models/income_expected.go
+++ models/income_expected.go
@@ -1,4 +1,17 @@
 package models
 
+import (
+	"time"
+)
+
 type IncomeExpected struct {
+	id_accaunt       int64
+	id_income_ex     int64
+	amount           float64
+	type_income      string //salary or award
+	income_mont_date int    //1-31
+
+	u_by             string    //who changed
+	date_actual_from time.Time //actual from
+	date_actual_to   time.Time //actual to if 9999-12-31 to nowday
 }
